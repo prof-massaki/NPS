b44fef70962010608a313b0d7716012e78581aef
prof-massaki <massaki.igarashi@sp.senai.br>
2024-08-30 13:03:29 -0300

    Update StreamlitFORM.py

--- StreamlitFORM.py
+++ StreamlitFORM.py
@@ -4,41 +4,14 @@ from io import BytesIO
 import requests
 import pandas as pd
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-from email.mime.text import MIMEText
-
-from urllib.request import urlopen
-
-def SendMAIL(assunto):
-    # create message object instance 
-    msg = MIMEMultipart()     
-    # setup the parameters of the message 
-    password = "makchgxvssjzogky"
-    msg['From'] = "prof.massaki@gmail.com"
-    msg['To'] =   "actspsolucoesparapessoas+cgaew9i3s6kcacagaoeg@boards.trello.com"
-    msg['Subject'] = assunto
-    #file = "Python.pdf"
-    # attach image to message body 
-    #msg.attach(MIMEText(open(file).read()))     
-    # create server 
-    server = smtplib.SMTP('smtp.gmail.com: 465')     
-    server.starttls()     
-    # Login Credentials for sending the mail 
-    server.login(msg['From'], password)      
-    # send the message via the server. 
-    server.sendmail(msg['From'], msg['To'], msg.as_string("MENSAGEM TESTE"))     
-    server.quit()
-
 st.set_page_config(
      page_title="Formulário de Cadastro",
      page_icon=":date:",
      layout="wide",
      initial_sidebar_state="expanded",
      menu_items={
-         'Get Help': 'mailto:informacoes.actsp@gmail.com',
-         'Report a bug': "mailto:informacoes.actsp@gmail.com",
+         'Get Help': 'mailto:prof.massaki@gmail.com',
+         'Report a bug': "mailto:prof.massaki@gmail.com",
          'About': "#### Desenvolvedor: Massaki de O. Igarashi"
      }
  )
